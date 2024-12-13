e2bdfaa21a8433ffc75d4a6806014547b955f119
Your Name <msksurjith@gmail.com>
2024-12-13 18:57:55 +0530

    ch

--- runner.go
+++ runner.go
@@ -7,10 +7,17 @@ import (
 )
 
 func greet(w http.ResponseWriter, r *http.Request) {
+
 	fmt.Fprintf(w, "Hello World! %s", time.Now())
 }
 
 func main() {
 	// http.HandleFunc("/", greet)
 	// http.ListenAndServe(":8080", nil)
+	var one, two, three, four, five int := 1, 2, 3, 4, 5
+	fmt.Println(one)
+	fmt.Println(two)
+	fmt.Println(three)
+	fmt.Println(four)
+	fmt.Println(five)
 }
