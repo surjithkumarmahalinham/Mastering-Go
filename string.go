e590f4e3f268c6b80170d9c01dda7911c74a0595
Your Name <msksurjith@gmail.com>
2024-12-18 18:32:37 +0530

    contains

--- string.go
+++ string.go
@@ -9,6 +9,10 @@ func main() {
 	msg := "one"
 	msg2 := "s"
 
+	contain := "golang programming language"
+	fmt.Println(strings.Contains(contain, "golang"))
+
 	fmt.Println(strings.Compare(msg, msg2))
+
 	fmt.Println(len(msg))
 }
