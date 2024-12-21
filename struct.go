3513bdc9ab5b60ec3fba6a574b304bb7dfe27c44
Your Name <msksurjith@gmail.com>
2024-12-21 18:37:03 +0530

    changes

---
+++ struct.go
@@ -0,0 +1,26 @@
+// A struct is used to create a collection of members of different data types, into a single variable.
+
+package main
+
+import "fmt"
+
+type Person struct {
+	name    string
+	age     int
+	address string
+}
+
+type Programmer struct {
+	Person
+	isProgrammer bool
+}
+
+func main() {
+	h := Person{
+		name:    "msk",
+		age:     28,
+		address: "chennai",
+	}
+
+	fmt.Println(h)
+}
