360220dd8c3881b68a8e7139cc138257a9a8bbe2
Your Name <msksurjith@gmail.com>
2024-12-24 16:12:54 +0530

    interface

--- interface.go
+++ interface.go
@@ -1,1 +1,35 @@
 package main
+
+import "fmt"
+
+type Animal interface {
+	GetInfo() string
+}
+
+type Cat struct {
+	Name  string
+	Color string
+}
+
+func (c Cat) GetInfo() string {
+	return fmt.Sprintf("Cat:%s, Color:%s", c.Name, c.Color)
+}
+
+type Dog struct {
+	Name  string
+	Breed string
+}
+
+func (d Dog) GetInfo() string {
+	return fmt.Sprintf("Dog: %s, Breed: %s", d.Name, d.Breed)
+}
+func printAnimalInfo(animal Animal) {
+	fmt.Println(animal.GetInfo())
+}
+
+func main() {
+	cat := Cat{Name: "Jack", Color: "Gray"}
+	dog := Dog{Name: "Domy", Breed: "Labaror"}
+	printAnimalInfo(cat)
+	printAnimalInfo(dog)
+}
