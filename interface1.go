9f8ffdb7fb6b53fc58c53612afc9ad1859c5a273
Your Name <msksurjith@gmail.com>
2024-12-31 11:10:00 +0530

    completed

--- interface1.go
+++ interface1.go
@@ -1,5 +1,7 @@
 package main
 
+import "log"
+
 // Define interface
 type Human interface {
 	Username() string
@@ -36,7 +38,7 @@ func (a User2) Username() string {
 
 // User Information
 func PrintInfo(h Human) {
-
+	log.Println("My name is a ", h.Username(), " and My Profession is ", h.Profession())
 }
 
 func main() {
