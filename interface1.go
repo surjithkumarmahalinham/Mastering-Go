6af3d1a9fa855abc9f11ac5d271c8dba6574f6e7
Your Name <msksurjith@gmail.com>
2025-01-02 19:23:41 +0530

    ch

--- interface1.go
+++ interface1.go
@@ -1,7 +1,5 @@
 package main
 
-import "log"
-
 // Define interface
 type Human interface {
 	Username() string
@@ -37,9 +35,6 @@ func (a User2) Username() string {
 }
 
 // User Information
-func PrintInfo(h Human) {
-	log.Println("My name is a ", h.Username(), " and My Profession is ", h.Profession())
-}
 
 func main() {
 	// Instance of struct
