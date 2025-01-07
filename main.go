3ffaaa650660e5436b69f29bd880f957ed8f0b8e
Your Name <msksurjith@gmail.com>
2025-01-07 19:48:50 +0530

    ch

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/", fileServer)
+	http.Handle("/form", formHandler)
+	http.Handle("/hello", helloHandler)
+
+	fmt.Println("Starting server at port 8008")
+
+	if err := http.ListenAndServe(":8008", nil); err != nil {
+		log.Fatal(err)
+	}
+}
