4cac298016b87b2e88bbb68bddfb221689013c9d
Your Name <msksurjith@gmail.com>
2025-01-08 19:03:02 +0530

    dn

--- main.go
+++ main.go
@@ -16,7 +16,8 @@ func main() {
 	http.Handle("/form", formHandler)
 	http.Handle("/hello", helloHandler)
 
-	
+	fmt.Println("Starting server at port 8008")
+
 	if err := http.ListenAndServe(":8008", nil); err != nil {
 		log.Fatal(err)
 	}
