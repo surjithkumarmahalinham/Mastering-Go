b55beeafab717c901df73b7c09f5b399bb8deda6
Your Name <msksurjith@gmail.com>
2025-01-09 19:10:16 +0530

    changes

--- main.go
+++ main.go
@@ -6,15 +6,35 @@ import (
 	"net/http"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Println(w, "Parseform errror: %v", err)
+	}
+	fmt.Println(w, "post request success")
+	fname := r.FormValue("fname")
+	lname := r.FormValue("lname")
+
+	fmt.Println(w, "FName = %s", fname)
+	fmt.Println(w, "LName = %s", lname)
+
+}
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	if(r.URL.Path !=)
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method not support", http.StatusNotFound)
+		return
+	}
+	fmt.Println(w, "hello!")
 }
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 
 	http.Handle("/", fileServer)
 	http.Handle("/form", formHandler)
-	http.Handle("/hello", helloHandler)
+	http.Handle("/hello", formHandler)
 
 	fmt.Println("Starting server at port 8008")
 
