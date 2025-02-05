cdafd31e04355e21da4644eb66b9077529e60e77
Your Name <msksurjith@gmail.com>
2025-02-05 19:00:10 +0530

    dd

--- main.go
+++ main.go
@@ -14,9 +14,6 @@ func formValidate(w http.ResponseWriter, r *http.Request) {
 	fname := r.FormValue("fname")
 	lname := r.FormValue("lname")
 
-	fmt.Println(w, "FName = %s", fname)
-	fmt.Println(w, "LName = %s", lname)
-
 }
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
